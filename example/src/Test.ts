e76eef57ca1f8c7e07bebc95401dfd4f246948aa
calico-crusade <admin@index-0.com>
2019-02-14 16:49:41 -0500

    automatic commit

---
+++ example/src/Test.ts
@@ -0,0 +1,33 @@
+import { PalBot, Plugin, PalringoClient, ExtendedMessage } from 'palringo-ts';
+
+class Test {
+
+    public bot: PalringoClient;
+
+    @Plugin('test', {
+        aliases: ['t', '-t', '--test']
+    })
+    static testPlugin(client: PalringoClient, msg: ExtendedMessage) {
+        client.reply(msg, "Hello how are you?");
+        client.privateMessage(msg.userProfile.id, 'hello world');
+
+        if (msg.isGroup)
+            client.groupMessage(msg.group.id, 'hello world');
+
+        var gr = client.Info.Groups.get(msg.group.id);
+        
+    }
+
+    start() {
+        this.bot = PalBot();
+        this.bot.registerPlugins('!ts');
+        this.bot.On.Log = (item) => console.log('Item logged', item);
+        this.bot.On.LoginSuccess = (user) => console.log('User logged in: ', user.nickname);
+        this.bot.On.LoginFailed = (reason) => console.log('Login Failed: ', reason);
+        this.bot.On.Disconnected = () => console.log('Disconnected');
+        this.bot.On.Connected = () => console.log('Connected');
+        this.bot.login('roulettetest@palbot.com', 'asdf');
+    }
+}
+
+new Test().start();
